88ce8c1a93dcf91864923c4ee97b8d9f358024cc
sosenx <barteksosnowski711@gmail.com>
2018-04-07 15:48:22 +0200

    base ui styles done

--- templates/x-markups-input-form.php
+++ templates/x-markups-input-form.php
@@ -1,24 +1,23 @@
 <div class="x-markups-input-form">
 
 
+	<div class="gcalc_fgroup">
+		<h3 class="gcalc_fgroup__label">{{$root.__tr('Markups settings')}}</h3>
 
-<div class="gcalc_fgroup">
-	<h3 class="gcalc_fgroup__label">{{$root.__tr('Markups settings')}}</h3>
+		<ul>
+			<li v-for="( markup_part, key ) in markups_changes" v-if="markup_part.enabled">
+				<label :for="key">{{ $root.__tr( typeof markup_part.label === 'string' ? markup_part.label : key ) }}</label>
+				<b-form-input 
+					:id			="'markup-input-' + key"				
+					v-on:change	="markups_changed" 
+					v-model		="markups_[key]"
+				    :type		="markup_part.field_type"
+				></b-form-input>
+				<label :for="key" class="unit">%</label>
+			</li>
+		</ul>	
 
-<ul>
-	<li v-for="( markup_part, key ) in markups_changes" v-if="markup_part.enabled">
-		<label :for="key">{{ $root.__tr( typeof markup_part.label === 'string' ? markup_part.label : key ) }}</label>
-		<b-form-input 
-			:id			="'markup-input-' + key"				
-			v-on:change	="markups_changed" 
-			v-model		="markups_[key]"
-		    :type		="markup_part.field_type"
-		></b-form-input>
-		<label :for="key" class="unit">%</label>
-	</li>
-</ul>	
-
-       </div>
+    </div>
 
 
 </div>
